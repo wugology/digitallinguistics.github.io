d98d0440696d11701a7232c36316634d4b326995
John <john.m5321@yahoo.com>
2015-05-02 19:01:51 -0700

    Uses localStorage to limit intro.js use in homepage
    Sets localStorage variables and if statements that check for them, so
    that the homepage runs homePageIntro only when the introdemo has not
    been completed

--- js/demo.js
+++ js/demo.js
@@ -157,6 +157,8 @@ function runDemo() {
         updatePhrase();
         updateWords();
       }
+  }).oncomplete(function(){
+    localStorage.setItem('introFinished',true);
   }).start();
 }
 // Event listeners
